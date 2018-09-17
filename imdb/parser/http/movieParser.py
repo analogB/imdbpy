7300427bc460cec5e3ac3dbf1ddc91648b8c8585
brad rogers <bradrogersau@gmail.com>
2018-09-17 10:46:14 -0400

    testing "text show-more__control" for review cont

--- imdb/parser/http/movieParser.py
+++ imdb/parser/http/movieParser.py
@@ -1590,7 +1590,7 @@ class DOMHTMLReviewsParser(DOMParserBase):
                 rules=[
                     Rule(
                         key='text',
-                        extractor=Path('.//div[@class="text"]//text()')
+                        extractor=Path('.//div[@class="text show-more__control"]//text()') #extractor=Path('.//div[@class="text"]//text()')
                     ),
                     Rule(
                         key='helpful',
