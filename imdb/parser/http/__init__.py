08215cae735ccff8e30afb3cd1675a29f63e5400
brad rogers <bradrogersau@gmail.com>
2018-09-19 13:55:50 -0400

    Update __init__.py

--- imdb/parser/http/__init__.py
+++ imdb/parser/http/__init__.py
@@ -524,8 +524,12 @@ class IMDbHTTPAccessSystem(IMDbBase):
         self.driver = webdriver.Chrome("/Users/brad/Downloads/chromedriver")
         self.driver.get(url)
         
+        p = 0
         while self.driver.find_elements_by_css_selector('.ipl-load-more__button'):
+            print(p)
             self.driver.find_element_by_css_selector('.ipl-load-more__button').click()
+            print(p)
+            p+=1
         
         #cont = self._retrieve(url) 
         #cont = self._retrieve(self.urls['movie_main'] % movieID + 'reviews?count=9999999&start=0')
